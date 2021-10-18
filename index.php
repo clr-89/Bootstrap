b02b8347309d5f228ec05162537c909bcc9a5d03
clr-89 <74655761+clr-89@users.noreply.github.com>
2021-10-18 10:58:32 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,76 @@
+
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Titre de la page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-1 bg-black">.</div>
+            <div class="col-1 offset-9 bg-black">.</div>
+        </div>
+        <div class="row">
+            <div class="col-1 bg-black">.</div>
+            <div class="col-1 offset-3 bg-danger">.</div>
+            <div class="col-1 bg-warning">.</div>
+            <div class="col-1 bg-danger">.</div>
+            <div class="col-1 offset-3 bg-black">.</div>
+        </div>
+        <div class="row">
+            <div class="col-1 bg-black">.</div>
+            <div class="col-1 offset-2 bg-danger">.</div>
+            <div class="col-1 bg-warning">.</div>
+            <div class="col-1 bg-black">.</div>
+            <div class="col-1 bg-warning">.</div>
+            <div class="col-1 bg-danger">.</div>
+            <div class="col-1 offset-2 bg-black">.</div>
+        </div>
+        <div class="row">
+            <div class="col-2 bg-black">.</div>
+            <div class="col-2 bg-danger">.</div>
+            <div class="col-1 bg-warning">.</div>
+            <div class="col-1 bg-black">.</div>
+            <div class="col-1 bg-warning">.</div>
+            <div class="col-2 bg-danger">.</div>
+            <div class="col-2 bg-black">.</div>
+        </div>
+        <div class="row">
+            <div class="col-2 bg-black">.</div>
+            <div class="col-1 offset-1 bg-danger">.</div>
+            <div class="col-1 bg-warning">.</div>
+            <div class="col-1 bg-black">.</div>
+            <div class="col-1 bg-warning">.</div>
+            <div class="col-1 bg-danger">.</div>
+            <div class="col-2 offset-1 bg-black">.</div>
+        </div>
+        <div class="row">
+            <div class="col-3 bg-black">.</div>
+            <div class="col-1 offset-1 bg-danger">.</div>
+            <div class="col-1 bg-warning">.</div>
+            <div class="col-1 bg-danger">.</div>
+            <div class="col-3 offset-1 bg-black">.</div>
+        </div>
+        <div class="row">
+            <div class="col-3 offset-1 bg-black">.</div>
+            <div class="col-3 offset-3 bg-black">.</div>
+        </div>
+        <div class="row">
+            <div class="col-7 offset-2 bg-black">.</div>
+        </div>
+        <div class="row">
+            <div class="col-5 offset-3 bg-black">.</div>
+        </div>
+        <div class="row">
+            <div class="col-3 offset-4 bg-black">.</div>
+        </div>
+        <div class="row">
+            <div class="col-3 offset-4 bg-black">.</div>
+        </div>
+    </div>
+
+</body>
+</html>
